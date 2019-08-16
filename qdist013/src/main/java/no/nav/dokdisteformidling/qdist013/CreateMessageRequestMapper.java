15f15ed5797cf00934130d61aadee009fed3b800
Sigurd Midttun <sigurd.midttun@nav.no>
2019-08-16 15:35:18 +0200

    MMA-3309 Implementert mappertest

--- qdist013/src/main/java/no/nav/dokdisteformidling/qdist013/CreateMessageRequestMapper.java
+++ qdist013/src/main/java/no/nav/dokdisteformidling/qdist013/CreateMessageRequestMapper.java
@@ -28,6 +28,7 @@ public class CreateMessageRequestMapper {
 	private static final String CONVERSATION_ID = "ConversationId";
 	private static final String SENDER_REF = "SenderRef";
 	private static final String RECEIVER_REF = "ReceiverRef";
+	private static final OffsetDateTime NOW = OffsetDateTime.now();
 
 	public CreateMessageRequest map(String conversationId, HentForsendelseResponseTo hentForsendelseResponseTo) {
 		return CreateMessageRequest.builder()
@@ -47,7 +48,7 @@ public class CreateMessageRequestMapper {
 										.instanceIdentifier(konversasjonsId)
 										.scopeInformation(new HashSet<>(Collections.singletonList(CreateMessageRequest.StandardBusinessDocumentHeader.BusinessScope.Scope.CorrelationInformation
 												.builder()
-												.expectedResponseDateTime(OffsetDateTime.now().plus(4, HOURS))
+												.expectedResponseDateTime(NOW.plus(4, HOURS))
 												.build())))
 										.type(CONVERSATION_ID)
 										.build(),
@@ -62,6 +63,7 @@ public class CreateMessageRequestMapper {
 						)))
 						.build())
 				.documentIdentification(CreateMessageRequest.StandardBusinessDocumentHeader.DocumentIdentification.builder()
+						.creationDateAndTime(NOW)
 						.instanceIdentifier(hentForsendelseResponseTo.getBestillingsId())
 						.multipleType(Boolean.TRUE)
 						.standard(DOCUMENT_IDENTIFICATOR_STANDARD)
