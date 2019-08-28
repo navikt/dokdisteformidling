0f669bf86970a15defa28571ea0b1ec00d412b9c
Erik Braten <erik.braten@nav.no>
2019-08-28 15:38:19 +0200

    MMA-2887 Bruk leader election så kun en pod oppdaterer eformidlingstatus

---
+++ core/src/main/java/no/nav/dokdisteformidling/consumer/leaderelection/LeaderElection.java
@@ -0,0 +1,8 @@
+package no.nav.dokdisteformidling.consumer.leaderelection;
+
+/**
+ * @author Erik Bråten, Visma Consulting
+ */
+public interface LeaderElection {
+	boolean isLeader();
+}
--- sdist001/src/main/java/no/nav/dokdisteformidling/sdist001/Sdist001Scheduled.java
+++ sdist001/src/main/java/no/nav/dokdisteformidling/sdist001/Sdist001Scheduled.java
@@ -1,6 +1,7 @@
 package no.nav.dokdisteformidling.sdist001;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.dokdisteformidling.consumer.leaderelection.LeaderElection;
 import no.nav.dokdisteformidling.consumer.rdist001.AdministrerForsendelse;
 import no.nav.dokdisteformidling.consumer.rdist001.HentEformidlingforsendelserResponseTo;
 import no.nav.dokdisteformidling.exception.functional.AbstractDokdisteformidlingFunctionalException;
@@ -19,14 +20,23 @@ public class Sdist001Scheduled {
 
 	private final AdministrerForsendelse administrerForsendelse;
 	private final Sdist001Service sdist001Service;
+	private final LeaderElection leaderElection;
 
 	public Sdist001Scheduled(AdministrerForsendelse administrerForsendelse,
-							 Sdist001Service sdist001Service) {
+							 Sdist001Service sdist001Service,
+							 LeaderElection leaderElection) {
 		this.administrerForsendelse = administrerForsendelse;
 		this.sdist001Service = sdist001Service;
+		this.leaderElection = leaderElection;
 	}
 
 	@Scheduled(fixedDelayString = "${sdist001.intervall?:600000}")
+	public void triggerOppdatering() {
+		if (leaderElection.isLeader()) {
+			oppdaterEformidlingStatus();
+		}
+	}
+
 	@Monitor(value = "dok_metric", extraTags = {"process", "oppdaterEformidlingStatus"}, histogram = true)
 	public void oppdaterEformidlingStatus() {
 		log.info("sdist001 oppdaterer status for eFormidlingforsendelser");
