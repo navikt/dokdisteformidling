a076bc329eb59c92e1de8a05bd19f75adde80962
Heidi Elisabeth Sando <heidi.elisabeth.sando@nav.no>
2020-01-22 12:26:55 +0100

    Prøver nye ting for deploy

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -5,21 +5,21 @@ ls -la /secrets/serviceuser
 
 if test -f /secrets/serviceuser/srvdokdisteformidling/username;
 then
-    echo "Setting serviceuser.username"
-    export  serviceuser.username=$(cat /secrets/serviceuser/srvdokdisteformidling/username)
+    echo "Setting serviceuser_username"
+    export  serviceuser_username=$(cat /secrets/serviceuser/srvdokdisteformidling/username)
 fi
 if test -f /secrets/serviceuser/srvdokdisteformidling/password;
 then
-    echo "Setting serviceuser.password"
-    export  serviceuser.***passord=gammelt_passord***)
+    echo "Setting serviceuser_password"
+    export  serviceuser_***passord=gammelt_passord***)
 fi
 if test -f /secrets/privateKey/privateKeyFile;
 then
-    echo "Setting sftp.privateKey"
-    export  sftp.privateKey=$(cat /secrets/privateKey/privateKeyFile)
+    echo "Setting sftp_privateKey"
+    export  sftp_privateKey=$(cat /secrets/privateKey/privateKeyFile)
 fi
 if test -f /secrets/privateKey/privateKeyPassphrase;
 then
-    echo "Setting sftp.privateKeyPassphrase"
-    export  sftp.privateKeyPassphrase=$(cat /secrets/privateKey/privateKeyPassphrase)
+    echo "Setting sftp_privateKeyPassphrase"
+    export  sftp_privateKeyPassphrase=$(cat /secrets/privateKey/privateKeyPassphrase)
 fi
