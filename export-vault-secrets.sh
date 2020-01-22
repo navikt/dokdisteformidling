79d5c63743133e77661df7e7c3cf1986a7a95802
Heidi Elisabeth Sando <heidi.elisabeth.sando@nav.no>
2020-01-22 13:28:10 +0100

    Prøver nye ting for deploy

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -22,8 +22,9 @@ if test -d /var/run/secrets/nais.io/vault;
 then
     echo "Setting sftp_username"
     export  sftp_username=$(cat /var/run/secrets/nais.io/vault/sftp_username)
-    echo "Setting dokdisteformidling_S3_creds_username"
-    export  dokdisteformidling_S3_creds_username=$(cat /var/run/secrets/nais.io/vault/dokdisteformidling_S3_creds_username)
+    echo "Setting dokdisteformidling_s3_creds_username"
+    export  dokdisteformidling_s3_creds_username=$(cat /var/run/secrets/nais.io/vault/dokdisteformidling_s3_creds_username)
+    echo $dokdisteformidling_s3_creds_username
     echo "Setting dokdisteformidling_S3_creds_password"
     export  dokdisteformidling_S3_creds_***passord=gammelt_passord***)
     echo "Setting dokdistmellomlager_s3_storage_crypto_password"
