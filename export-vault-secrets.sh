1a3f55e87c0fe9979257ddb0965e62154f9ce42e
Heidi Elisabeth Sando <heidi.elisabeth.sando@nav.no>
2020-01-22 10:59:47 +0100

    Fikset feil på init-script

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -2,21 +2,21 @@
 
 if test -f /var/run/secrets/nais.io/srvdokdisteformidling/username;
 then
-    echo "Setting serviceuser_username"
-    export  serviceuser_username=$(cat /var/run/secrets/nais.io/srvdokdisteformidling/username)
+    echo "Setting serviceuser.username"
+    export  serviceuser.username=$(cat /var/run/secrets/nais.io/srvdokdisteformidling/username)
 fi
 if test -f /var/run/secrets/nais.io/srvdokdisteformidling;
 then
-    echo "Setting serviceuser_password"
-    export  serviceuser_***passord=gammelt_passord***)
+    echo "Setting serviceuser.password"
+    export  serviceuser.***passord=gammelt_passord***)
 fi
 if test -f /secrets/privateKey/privateKeyFile;
 then
-    echo "Setting sftp_privateKey"
-    export  sftp_privateKey=$(cat /secrets/privateKey/privateKeyFile)
+    echo "Setting sftp.privateKey"
+    export  sftp.privateKey=$(cat /secrets/privateKey/privateKeyFile)
 fi
 if test -f /secrets/privateKey/privateKeyPassphrase;
 then
-    echo "Setting sftp_privateKeyPassphrase"
-    export  sftp_privateKeyPassphrase=$(cat /secrets/privateKey/privateKeyPassphrase)
+    echo "Setting sftp.privateKeyPassphrase"
+    export  sftp.privateKeyPassphrase=$(cat /secrets/privateKey/privateKeyPassphrase)
 fi
