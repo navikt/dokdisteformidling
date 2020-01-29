c9cba133c5d8fbf1ff5a3ced454ad3feee164d80
Tsigab Angosom Gebremedhin <Tsigab.Angosom.Gebremedhin@nav.no>
2020-01-29 17:44:25 +0100

    MMA-4040 lagt til config fil til application.itest.proporties

--- sdist001/src/test/java/no/nav/dokdisteformidling/sdist001/itest/config/ApplicationTestConfig.java
+++ sdist001/src/test/java/no/nav/dokdisteformidling/sdist001/itest/config/ApplicationTestConfig.java
@@ -5,8 +5,7 @@ import static org.mockito.Mockito.mock;
 import no.nav.dokdisteformidling.certificate.KeyStoreProperties;
 import no.nav.dokdisteformidling.config.alias.DigitalKontaktinformasjonV1Alias;
 import no.nav.dokdisteformidling.config.alias.ServiceuserAlias;
-import no.nav.dokdisteformidling.config.props.MaskinportenProperties;
-import no.nav.dokdisteformidling.config.props.ServiceRegistryProperties;
+import no.nav.dokdisteformidling.config.props.*;
 import no.nav.dokdisteformidling.config.sts.STSConfig;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +18,9 @@ import org.springframework.context.annotation.Profile;
  */
 @Configuration
 @Profile("itest")
-@EnableConfigurationProperties({ServiceuserAlias.class,
+@EnableConfigurationProperties({ServiceuserAlias.class, DpoUserProperties.class,
+		BrokerServiceExternalEC2Properties.class,
+		BrokerServiceExternalECStreamedProperties.class,
 		KeyStoreProperties.class,
 		DigitalKontaktinformasjonV1Alias.class,
 		MaskinportenProperties.class,
