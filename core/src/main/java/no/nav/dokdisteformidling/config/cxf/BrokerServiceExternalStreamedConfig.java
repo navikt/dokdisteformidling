23a5697a08ed36efc8d70650dc651a87e2801b6e
Mårten Elmgren (E147566) <marten.samuel.elmgren@nav.no>
2020-01-31 13:49:16 +0100

    Konfigurere BrokerServiceExternalStreamedConfig

---
+++ core/src/main/java/no/nav/dokdisteformidling/config/cxf/BrokerServiceExternalStreamedConfig.java
@@ -0,0 +1,61 @@
+package no.nav.dokdisteformidling.config.cxf;
+
+import no.altinn.brokerserviceexternalstreamed.BrokerServiceExternalStreamedSF;
+import no.altinn.brokerserviceexternalstreamed.IBrokerServiceExternalStreamed;
+import no.nav.dokdisteformidling.config.interceptor.ClientCallBackHandler;
+import no.nav.dokdisteformidling.config.interceptor.CookiesInInterceptor;
+import no.nav.dokdisteformidling.config.interceptor.CookiesOutInterceptor;
+import no.nav.dokdisteformidling.config.interceptor.HeaderOutInterceptor;
+import no.nav.dokdisteformidling.config.props.BrokerServiceExternalStreamedProperties;
+import no.nav.dokdisteformidling.config.props.DpoUserProperties;
+import org.apache.cxf.Bus;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+@Configuration
+public class BrokerServiceExternalStreamedConfig extends AbstractCxfEndpointConfig {
+	@Inject
+	public BrokerServiceExternalStreamedConfig(Bus bus) {
+		super(bus);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Bean
+	public IBrokerServiceExternalStreamed iBrokerServiceExternalStreamed(BrokerServiceExternalStreamedProperties brokerServiceExternalStreamedProperties,
+																		 DpoUserProperties dpoUserProperties) throws IOException {
+		setWsdlUrl("wsdl/BrokerServiceExternalStreamed.wsdl");
+		setServiceName(BrokerServiceExternalStreamedSF.SERVICE);
+		setEndpointName(BrokerServiceExternalStreamedSF.CustomBindingIBrokerServiceExternalStreamed);
+		setAddress(brokerServiceExternalStreamedProperties.getEndpointurl());
+		setReceiveTimeout(brokerServiceExternalStreamedProperties.getReadtimeoutms());
+		setConnectTimeout(brokerServiceExternalStreamedProperties.getConnecttimeoutms());
+
+		addInInterceptor(new LoggingInInterceptor());
+		addOutInterceptor(new LoggingOutInterceptor());
+
+		addInInterceptor(new CookiesInInterceptor());
+		addOutInterceptor(new HeaderOutInterceptor());
+		addOutInterceptor(new CookiesOutInterceptor());
+
+		IBrokerServiceExternalStreamed iBrokerServiceExternalStreamed = createPort(IBrokerServiceExternalStreamed.class);
+		final Client client = ClientProxy.getClient(iBrokerServiceExternalStreamed);
+		setRequestContext(client, dpoUserProperties);
+		return iBrokerServiceExternalStreamed;
+	}
+
+	// TODO: Vurdere gjennbruk av konfiguerering
+	private void setRequestContext(final Client client, DpoUserProperties dpoUserProperties) {
+		client.getRequestContext().put("ws-security.must-understand", Boolean.TRUE);
+		client.getRequestContext().put("ws-security.username", dpoUserProperties.getUsername());
+		client.getRequestContext().put("ws-security.callback-handler", new ClientCallBackHandler(dpoUserProperties));
+		client.getRequestContext().put("org.apache.cxf.message.Message.MAINTAIN_SESSION", Boolean.TRUE);
+		client.getRequestContext().put("javax.xml.ws.session.maintain", Boolean.TRUE);
+	}
+}
