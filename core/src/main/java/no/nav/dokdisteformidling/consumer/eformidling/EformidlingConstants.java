240a3c749e949e62d70980b8748cd15168bdea37
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2020-02-04 12:10:36 +0100

    MMA-4124 Endret orgnr tilbake til trygderetten sitt

--- core/src/main/java/no/nav/dokdisteformidling/consumer/eformidling/EformidlingConstants.java
+++ core/src/main/java/no/nav/dokdisteformidling/consumer/eformidling/EformidlingConstants.java
@@ -5,9 +5,7 @@ package no.nav.dokdisteformidling.consumer.eformidling;
  */
 public final class EformidlingConstants {
 	public static final String NAV_ORGNUMMER = "889640782"; // hardkodet inntil videre fordi DPO-bruker er knyttet til denne (MMA-3834)
-	// TODO teknisk verifisering. Trygderetten sitt orgnummer er 974761084. Hvis orgnummer er noe annet så er det en feil.
-	// TODO difi test org 987464291
-	public static final String TRYGDERETTEN_ORGNUMMER = "987464291";
+	public static final String TRYGDERETTEN_ORGNUMMER = "974761084";
 	public static final String ARKIVMELDING_PROCESS = "urn:no:difi:profile:arkivmelding:administrasjon:ver1.0";
 
 	private EformidlingConstants() {
