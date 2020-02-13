f8779227175b66bae605d662457c9954fb43665c
Mårten Elmgren (E147566) <marten.samuel.elmgren@nav.no>
2020-02-13 12:52:28 +0100

    Bruker util klasser for Json og XML for å unmarshall inputStreams fra Altinn.

--- core/src/main/java/no/nav/dokdisteformidling/consumer/eformidling/dokumentpakker/EformidlingMessageUnpackager.java
+++ core/src/main/java/no/nav/dokdisteformidling/consumer/eformidling/dokumentpakker/EformidlingMessageUnpackager.java
@@ -1,6 +1,8 @@
 package no.nav.dokdisteformidling.consumer.eformidling.dokumentpakker;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.dokdisteformidling.common.JsonUtils;
+import no.nav.dokdisteformidling.common.XmlUtils;
 import no.nav.dokdisteformidling.consumer.eformidling.altinn.from.AltinnDokument;
 import no.nav.dokdisteformidling.consumer.eformidling.altinn.from.DownloadedMessageFromAltinn;
 import no.nav.dokdisteformidling.consumer.eformidling.dokumentpakker.brokerservicemanifest.BrokerServiceManifest;
@@ -8,9 +10,7 @@ import no.nav.dokdisteformidling.consumer.eformidling.dokumentpakker.exceptions.
 import no.nav.dokdisteformidling.consumer.eformidling.dokumentpakker.sbdh.StandardBusinessDocument;
 import org.springframework.stereotype.Component;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,18 +30,7 @@ import java.util.zip.ZipInputStream;
 public class EformidlingMessageUnpackager {
 
     private static final String IO_EXCEPTION = "Feil med IO ved unmarshalling";
-    private static final String JAXB_CONTEXTCREATION_EXCEPTION = "Kan ikke opprette JAXBContext: ";
     private static final String UNMARSHALLING_EXCEPTION = "Feil ved unmarshalling med JAXB";
-    private static JAXBContext jaxbContext;
-
-    static {
-        try {
-            jaxbContext = JAXBContext.newInstance(BrokerServiceManifest.class);
-        } catch (JAXBException e) {
-            log.error(JAXB_CONTEXTCREATION_EXCEPTION, e);
-            throw new DokumentpakkingException(JAXB_CONTEXTCREATION_EXCEPTION, e);
-        }
-    }
 
     public List<AltinnDokument> unpackageMessages(List<DownloadedMessageFromAltinn> messageFromAltinns) {
         List<AltinnDokument> altinnDokuments = new ArrayList<>();
@@ -63,16 +52,15 @@ public class EformidlingMessageUnpackager {
     private AltinnDokument unpack(DownloadedMessageFromAltinn melding) throws JAXBException, IOException {
         log.info("Pakker ut fil med referansenummer: " + melding.getFileReference().getFileReference());
         ZipInputStream zipInputStream = new ZipInputStream(melding.getInputStream());
-        StandardBusinessDocument sbd;
-        BrokerServiceManifest manifest;
 
+        BrokerServiceManifest manifest;
+        StandardBusinessDocument sbd;
         try (InputStream inputStreamProxy = new FilterInputStream(zipInputStream) {
             @Override
             public void close() {
                 // do nothing to avoid unmarshaller to close it before the Zip file is fully processed
             }
         }) {
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 
             ZipEntry zipEntry;
             manifest = null;
@@ -81,10 +69,10 @@ public class EformidlingMessageUnpackager {
             while ((zipEntry = zipInputStream.getNextEntry()) != null) {
                 switch (zipEntry.getName()) {
                     case AltinnDokument.MANIFEST_XML_FILENAME:
-                        manifest = (BrokerServiceManifest) unmarshaller.unmarshal(inputStreamProxy);
+                        manifest = XmlUtils.unmarshalXmlObject(inputStreamProxy, BrokerServiceManifest.class);
                         break;
                     case AltinnDokument.STANDARDBUSINESSDOCUMENT_JSON_FILENAME:
-                        sbd = (StandardBusinessDocument) unmarshaller.unmarshal(inputStreamProxy);
+                        sbd = JsonUtils.toObject(inputStreamProxy, StandardBusinessDocument.class);
                         break;
                     default:
                         log.info("Hopper over fil: {}", zipEntry.getName());
--- core/src/test/java/no/nav/dokdisteformidling/consumer/eformidling/dokumentpakker/EformidlingMessageUnpackagerTest.java
+++ core/src/test/java/no/nav/dokdisteformidling/consumer/eformidling/dokumentpakker/EformidlingMessageUnpackagerTest.java
@@ -20,12 +20,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class EformidlingMessageUnpackagerTest {
 
     private final static String SENDERS_REFERENCE = "33259df3-18ae-45e6-9861-47f42e119a14";
-    private final static String CONVERSATION_ID = "6205a3bb-e12a-4913-99db-877339e14496";
+    private final static String CONVERSATION_ID = "f1b3002b-1dea-4c14-8072-8c191183d04c";
 
     private final EformidlingMessageUnpackager eformidlingMessageUnpackager = new EformidlingMessageUnpackager();
     private int receiptId = 0;
-
-    private final static Long MILLIS_IN_A_DAY = 86400000L;
 
     @Test
     void shouldUnpackTestZipFile() {
@@ -36,14 +34,6 @@ public class EformidlingMessageUnpackagerTest {
 
         try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
             BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-
-//            ZipInputStream zipInputStream = new ZipInputStream(bufferedInputStream);
-//            ZipEntry zipEntry;
-//            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
-//                System.out.format("File: %s Size: %d Last Modified %s %n",
-//                        zipEntry.getName(), zipEntry.getSize(),
-//                        LocalDate.ofEpochDay(zipEntry.getTime() / MILLIS_IN_A_DAY));
-//            }
 
             messageFromAltinns.add(DownloadedMessageFromAltinn.builder()
                     .fileReference(createFileReference("fileReference"))
