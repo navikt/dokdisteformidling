93590028f1c0e5cc5531a204ae4e645143219923
Tsigab Angosom Gebremedhin <tsigb.angosom@gmail.com>
2020-05-05 17:26:08 +0200

    MMA-4356  refaktorert og la til mer itest for qdist013

--- qdist013/src/test/java/no/nav/dokdisteformidling/qdist013/itest/Qdist013ForAltinnIT.java
+++ qdist013/src/test/java/no/nav/dokdisteformidling/qdist013/itest/Qdist013ForAltinnIT.java
@@ -16,6 +16,7 @@ import org.apache.activemq.command.ActiveMQTextMessage;
 import org.apache.http.HttpHeaders;
 import org.apache.http.entity.ContentType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -178,11 +179,12 @@ public class Qdist013ForAltinnIT {
     }
 
     @Test
-    public void shouldIntiateBrokerServiceWhenDataJournalfoertErNull() {
+    @Order(1)
+    public void whenLightweightSafDataJournalfoertErNull() {
         stubGetForsendelse("__files/rjoark001/getForsendelse-happy.json");
         stubGetSecurityToken();
         stubPostSafJournalpost("queryJournalpostId\":\"123\"", "saf/safQdist013GraphQlResponse-aktoerId.json");
-        stubPostSafJournalpost("queryJournalpostId\":\"448212366\"", "saf/safLightweightGraphQlResponse-datojournalfoert-null.jsonn");
+        stubPostSafJournalpost("queryJournalpostId\":\"448212366\"", "saf/safLightweightGraphQlResponse-datojournalfoert-null.json");
         stubGetTpsHentPersonNavn("***gammelt_fnr***");
         stubGetAktoerregisterHentIdentForAktoerId("***gammelt_fnr***09");
         stubPostMaskinporten();
@@ -191,12 +193,27 @@ public class Qdist013ForAltinnIT {
         sendStringMessage(qdist013, classpathToString("qdist013/qdist013-happy.xml"));
 
         await().atMost(10, SECONDS).untilAsserted(() -> {
-            verifyGetForsendelse();
-            verifyGetSecurityToken(4);
-            verifyPostSafJournalpost();
-            verify(1, postRequestedFor(urlEqualTo("/safgraphql")));
-
+            verifyAllStubsSaf("", "***gammelt_fnr***", "***gammelt_fnr***09", 17);
         });
+    }
+
+    @Test
+    @Order(2)
+    public void shouldThrowExceptionWhenDatoJournalfoertErNullInJpQdist013() {
+        stubGetForsendelse("__files/rjoark001/getForsendelse-happy.json");
+        stubGetSecurityToken();
+        stubPostSafJournalpost("queryJournalpostId\":\"123\"", "saf/safQdist013GraphQlResponse-relevantdato-null.json");
+        stubPostSafJournalpost("queryJournalpostId\":\"448212366\"", "saf/safLightweightGraphQlResponse-datojournalfoert-null.json");
+
+        sendStringMessage(qdist013, classpathToString("qdist013/qdist013-happy.xml"));
+
+        await().atMost(10, TimeUnit.SECONDS).untilAsserted(() -> {
+            assertMessageOnQueue(qdist013FunksjonellFeil);
+        });
+
+        verifyGetForsendelse();
+        verifyGetSecurityToken(1);
+        verifyPostSafJournalpost();
     }
 
     @Test
@@ -1028,6 +1045,29 @@ public class Qdist013ForAltinnIT {
         });
 
         verify(1, getRequestedFor(urlEqualTo("/administrerforsendelse/" + FORSENDELSE_ID)));
+    }
+
+
+    private void verifyAllStubsSaf(String orgnr, String fnr, String aktoerId, int stsCount) {
+        verifyGetForsendelse();
+        verifyGetSecurityToken(stsCount);
+        verifyPostSafJournalpost();
+        verify(14, postRequestedFor(urlEqualTo("/safgraphql"))
+                .withRequestBody(equalToJson(classpathToString("__files/saf/safLightweightGraphQlRequest.json"))));
+
+        if (!isNullOrEmpty(orgnr)) {
+            verifyGetEregHentOrgNavn(orgnr);
+        } else {
+            verifyGetTpsHentPersonNavn(fnr);
+        }
+        if (!isNullOrEmpty(aktoerId)) {
+            verifyGetAktoerregisterHentIdentForAktoerId(aktoerId);
+        }
+
+        verifyPostMaskinporten();
+        verifyGetServiceRegistry();
+        verifyPostIntiateBrokerService();
+
     }
 
 
