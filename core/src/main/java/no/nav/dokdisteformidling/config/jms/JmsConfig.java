650b2700d671ef830bf15e1920ede5e63d737f47
Tsigab Gebremedhin <tsigab.angosom@gmail.com>
2021-03-09 11:02:46 +0100

    MMA-5234 fikset etter pr kommentar

--- core/src/main/java/no/nav/dokdisteformidling/config/jms/JmsConfig.java
+++ core/src/main/java/no/nav/dokdisteformidling/config/jms/JmsConfig.java
@@ -25,63 +25,62 @@ import javax.jms.Queue;
 @Profile({"nais", "local"})
 public class JmsConfig {
 
-	private static final int UTF_8_WITH_PUA = 1208;
+    private static final int UTF_8_WITH_PUA = 1208;
 
-	@Bean
-	public Queue qdist011(@Value("${dokdisteformidling_qdist011_dist_til_dpi.queuename}") String qdist011QueueName) throws JMSException {
-		return new MQQueue(qdist011QueueName);
-	}
+    @Bean
+    public Queue qdist011(@Value("${dokdisteformidling_qdist011_dist_til_dpi.queuename}") String qdist011QueueName) throws JMSException {
+        return new MQQueue(qdist011QueueName);
+    }
 
-	@Bean
-	public Queue qdist013(@Value("${dokdisteformidling_qdist013_dist_trygderetten.queuename}") String qdist013QueueName) throws JMSException {
-		return new MQQueue(qdist013QueueName);
-	}
+    @Bean
+    public Queue qdist013(@Value("${dokdisteformidling_qdist013_dist_trygderetten.queuename}") String qdist013QueueName) throws JMSException {
+        return new MQQueue(qdist013QueueName);
+    }
 
-	@Bean
-	public Queue qdist011FunksjonellFeil(@Value("${dokdisteformidling_qdist011_funk_feil.queuename}") String qdist011FunksjonellFeil) throws JMSException {
-		return new MQQueue(qdist011FunksjonellFeil);
-	}
+    @Bean
+    public Queue qdist011FunksjonellFeil(@Value("${dokdisteformidling_qdist011_funk_feil.queuename}") String qdist011FunksjonellFeil) throws JMSException {
+        return new MQQueue(qdist011FunksjonellFeil);
+    }
 
-	@Bean
-	public Queue qdist013FunksjonellFeil(@Value("${dokdisteformidling_qdist013_funk_feil.queuename}") String qdist013FunksjonellFeil) throws JMSException {
-		return new MQQueue(qdist013FunksjonellFeil);
-	}
+    @Bean
+    public Queue qdist013FunksjonellFeil(@Value("${dokdisteformidling_qdist013_funk_feil.queuename}") String qdist013FunksjonellFeil) throws JMSException {
+        return new MQQueue(qdist013FunksjonellFeil);
+    }
 
-	@Bean
-	public Queue tdist005(@Value("${dokdistSdpBatchIntern.queuename}") String tdist005QueueName) throws JMSException {
-		return new MQQueue(tdist005QueueName);
-	}
+    @Bean
+    public Queue tdist005(@Value("${dokdistSdpBatchIntern.queuename}") String tdist005QueueName) throws JMSException {
+        return new MQQueue(tdist005QueueName);
+    }
 
-	@Bean
-	public ConnectionFactory wmqConnectionFactory(final MqGatewayAlias mqGatewayAlias,
-												  final @Value("${dokdisteformidling_channel.name}") String channelName,
-												  final ServiceuserAlias serviceuserAlias) throws JMSException {
-		return createConnectionFactory(mqGatewayAlias, channelName, serviceuserAlias);
-	}
+    @Bean
+    public ConnectionFactory wmqConnectionFactory(final MqGatewayAlias mqGatewayAlias,
+                                                  final @Value("${dokdisteformidling_channel.name}") String channelName,
+                                                  final ServiceuserAlias serviceuserAlias) throws JMSException {
+        return createConnectionFactory(mqGatewayAlias, channelName, serviceuserAlias);
+    }
 
-	private UserCredentialsConnectionFactoryAdapter createConnectionFactory(final MqGatewayAlias mqGatewayAlias,
-																			final String channelName,
-																			final ServiceuserAlias serviceuserAlias) throws JMSException {
-		MQConnectionFactory connectionFactory = new MQConnectionFactory();
-		connectionFactory.setHostName(mqGatewayAlias.getHostname());
-		connectionFactory.setPort(mqGatewayAlias.getPort());
-		connectionFactory.setChannel(channelName);
-		connectionFactory.setQueueManager(mqGatewayAlias.getName());
-		connectionFactory.setTransportType(WMQConstants.WMQ_CM_CLIENT);
-		connectionFactory.setCCSID(UTF_8_WITH_PUA);
-		connectionFactory.setIntProperty(WMQConstants.JMS_IBM_ENCODING, MQConstants.MQENC_NATIVE);
-		connectionFactory.setIntProperty(WMQConstants.JMS_IBM_CHARACTER_SET, UTF_8_WITH_PUA);
-		UserCredentialsConnectionFactoryAdapter adapter = new UserCredentialsConnectionFactoryAdapter();
-		adapter.setTargetConnectionFactory(connectionFactory);
-		if(mqGatewayAlias.isTlsbroker()) {
-			adapter.setUsername(serviceuserAlias.getUsername());
-			adapter.setPassword(serviceuserAlias.getPassword());
-		} else {
-			connectionFactory.setBooleanProperty(JmsConstants.USER_AUTHENTICATION_MQCSP, false);
-			adapter.setUsername(serviceuserAlias.getUsername());
-			adapter.setPassword(serviceuserAlias.getPassword());
-		}
+    private UserCredentialsConnectionFactoryAdapter createConnectionFactory(final MqGatewayAlias mqGatewayAlias,
+                                                                            final String channelName,
+                                                                            final ServiceuserAlias serviceuserAlias) throws JMSException {
+        MQConnectionFactory connectionFactory = new MQConnectionFactory();
+        connectionFactory.setHostName(mqGatewayAlias.getHostname());
+        connectionFactory.setPort(mqGatewayAlias.getPort());
+        connectionFactory.setChannel(channelName);
+        connectionFactory.setQueueManager(mqGatewayAlias.getName());
+        connectionFactory.setTransportType(WMQConstants.WMQ_CM_CLIENT);
+        connectionFactory.setCCSID(UTF_8_WITH_PUA);
+        connectionFactory.setIntProperty(WMQConstants.JMS_IBM_ENCODING, MQConstants.MQENC_NATIVE);
+        connectionFactory.setIntProperty(WMQConstants.JMS_IBM_CHARACTER_SET, UTF_8_WITH_PUA);
+        UserCredentialsConnectionFactoryAdapter adapter = new UserCredentialsConnectionFactoryAdapter();
+        adapter.setTargetConnectionFactory(connectionFactory);
+        if (!mqGatewayAlias.isTlsbroker()) {
+            connectionFactory.setBooleanProperty(JmsConstants.USER_AUTHENTICATION_MQCSP, false);
+        }
+        // Konfigurasjon for IBM MQ broker med TLS og autorisasjon med serviceuser mot onpremise Active Directory.
+        adapter.setUsername(serviceuserAlias.getUsername());
+        adapter.setPassword(serviceuserAlias.getPassword());
 
-		return adapter;
-	}
+
+        return adapter;
+    }
 }
