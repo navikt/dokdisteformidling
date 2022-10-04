31c4571f218a52ddcddc7a6e034076aa3ea7ff01
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2022-10-04 12:44:34 +0200

    MMA-6401 Forenklet sdist001 rapport

--- sdist001/src/main/java/no/nav/dokdisteformidling/sdist001/domain/ForsendelseStatusEndringer.java
+++ sdist001/src/main/java/no/nav/dokdisteformidling/sdist001/domain/ForsendelseStatusEndringer.java
@@ -2,7 +2,6 @@ package no.nav.dokdisteformidling.sdist001.domain;
 
 import lombok.NoArgsConstructor;
 import lombok.Value;
-import org.springframework.util.StringUtils;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -14,36 +13,10 @@ public class ForsendelseStatusEndringer {
 	Set<String> ekspedert = new HashSet<>();
 	Set<String> feilet = new HashSet<>();
 
+	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-
-		sb.append(getBekreftet().size());
-		sb.append(" forsendelser satt til bekreftet");
-		if (getBekreftet().size() > 0) {
-			sb.append(": " + StringUtils.collectionToCommaDelimitedString(getBekreftet()));
-		}
-		sb.append(". ");
-
-		sb.append(getEkspedert().size());
-		sb.append(" forsendelser satt til ekspedert");
-		if (getEkspedert().size() > 0) {
-			sb.append(": " + StringUtils.collectionToCommaDelimitedString(getEkspedert()));
-		}
-		sb.append(". ");
-
-		sb.append(getFeilet().size());
-		sb.append(" forsendelser satt til feilet");
-		if (getFeilet().size() > 0) {
-			sb.append(": " + StringUtils.collectionToCommaDelimitedString(getFeilet()));
-		}
-		sb.append(".");
-
-		return sb.toString();
-	}
-
-	public void clear() {
-		getBekreftet().clear();
-		getFeilet().clear();
-		getEkspedert().clear();
+		return """
+				sdist001 satte antall_bekreftet=%d, bekreftet=%s. antall_ekspedert=%d, ekspedert=%s. antall_feilet=%d, feilet=%s."""
+				.formatted(getBekreftet().size(), bekreftet, getEkspedert().size(), getEkspedert(), getFeilet().size(), getFeilet());
 	}
 }
