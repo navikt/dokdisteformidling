ac91076a5ec0e966de7a8caac3d76cd4b49ab322
Christian Strandenæs <Christian.Fredrik.Osterby.Strandenes@nav.no>
2023-11-24 17:02:26 +0100

    Revert "Håndterer en fortapt forsendelse, ved å bekrefte den hos Altinn"
    This reverts commit 36a19eeb175de4002d504e3a7b96e9f7da79e05b.

--- sdist001/src/main/java/no/nav/dokdisteformidling/sdist001/Sdist001Service.java
+++ sdist001/src/main/java/no/nav/dokdisteformidling/sdist001/Sdist001Service.java
@@ -73,7 +73,7 @@ public class Sdist001Service {
 							.filter(forsendelse -> validateForsendelse(forsendelse, downloadResponse))
 							.forEach(behandleForsendelse(downloadResponse, endringer));
 					if (validateForsendelse(FORTAPT_FORSENDELSE, downloadResponse)) {
-						eformidling.bekreft(downloadResponse.getFileReference());
+						behandleForsendelse(downloadResponse, endringer).accept(FORTAPT_FORSENDELSE);
 					}
 				});
 
